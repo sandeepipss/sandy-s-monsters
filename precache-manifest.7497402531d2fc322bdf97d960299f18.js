348b7a006f5a32a5643b59b51528806570e170c6
sandeep <sandeep.i.navale@intel.com>
2019-10-28 09:18:04 +0530

    Updates

---
+++ precache-manifest.7497402531d2fc322bdf97d960299f18.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d7b10f8173dfc9bac834c7ea27b3ccb",
+    "url": "/sandy-s-monsters/index.html"
+  },
+  {
+    "revision": "b9ad54a58746a3019488",
+    "url": "/sandy-s-monsters/static/css/main.6d7c4070.chunk.css"
+  },
+  {
+    "revision": "849a0e77c7a65ebc01db",
+    "url": "/sandy-s-monsters/static/js/2.64cdb3b7.chunk.js"
+  },
+  {
+    "revision": "b9ad54a58746a3019488",
+    "url": "/sandy-s-monsters/static/js/main.2a40d3b9.chunk.js"
+  },
+  {
+    "revision": "a954603348db2270e0e0",
+    "url": "/sandy-s-monsters/static/js/runtime-main.10500f53.js"
+  }
+]);
